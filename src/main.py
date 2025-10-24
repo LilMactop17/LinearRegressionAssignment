09e33f41428c6a6d51a7683b5f30f7fabd61a0f0
LilMactop17 <mohamed.tawfikomar@ontariotechu.net>
2025-10-23 22:24:06 -0500

    fixed dataset pat

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 #file location
-FILE_LOC = "Dataset.xlsx"
+FILE_LOC = "src/Dataset.xlsx"
 if os.path.exists(FILE_LOC):
     print("Dataset exists")
 SCALER = StandardScaler()
